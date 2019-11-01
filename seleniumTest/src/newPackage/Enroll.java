44304a754794a1e032b39c7fe1584650193081a9
JonathanShitrit <jonathanshitrit8@gmail.com>
2019-11-01 16:10:32 -0400

    Update Enroll.java

--- seleniumTest/src/newPackage/Enroll.java
+++ seleniumTest/src/newPackage/Enroll.java
@@ -4,11 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Enroll extends SeleniumFunctions {
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Enroll extends SeleniumFunctions {
 
